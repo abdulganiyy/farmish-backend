81069c0dcd44295a9a49bc44c129bffef4705f78
abdulganiyy <lasisibalogunopeoluwa@ymail.com>
2022-05-09 16:42:36 +0100

    added public folder

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const path = require("path");
 const app = express();
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use(express.static(path.join(__dirname + "/public")));
 
 app.use(cors());
 
